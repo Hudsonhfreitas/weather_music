3fa0ac6a67f6df1a6dcc533997e2f97a5fabbcef
Hudson Henrique de Freitas <hudson@MINERACAOCAFE.local>
2021-12-13 08:08:38 -0300

    api bug fixed

--- src/services/api.js
+++ src/services/api.js
@@ -16,7 +16,7 @@ export async function getCityData(city) {
 
 export async function getPlaylist(playlist) {
     const response = await axios.get(`https://shazam-core.p.rapidapi.com/v1/charts/genre-world`, {
-      params: {genre_code: playlist, limit: '18'},
+      params: {genre_code: playlist, limit: '20'},
       headers: {
         'x-rapidapi-host': 'shazam-core.p.rapidapi.com',
         'x-rapidapi-key': '7abb662075msh4ea11086b528ad8p10a1b8jsn83dc1d48996f'
